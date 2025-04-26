082df8e87d87385337d272787f45228ec4899c23
Kenny <kkha@indiana.edu>
2025-04-26 13:03:15 -0400

    Update Product model to be nullable

--- Models/Product.cs
+++ Models/Product.cs
@@ -3,9 +3,9 @@ namespace ECommerce.Models
     public class Product
     {
         public int Id { get; set; } // Primary Key
-        public string Name { get; set; }
+        public string? Name { get; set; }
         public decimal Price { get; set; }
-        public string Description { get; set; }
-        public string Category { get; set; }
+        public string? Description { get; set; }
+        public string? Category { get; set; }
     }
 }
