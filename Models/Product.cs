03e93a91b7633f00a7e08f987a58e62cb7eccecc
Kenny <kkha@indiana.edu>
2025-04-26 12:43:08 -0400

    Initial commit: Set up ECommerce project

---
+++ Models/Product.cs
@@ -0,0 +1,11 @@
+namespace ECommerce.Models
+{
+    public class Product
+    {
+        public int Id { get; set; } // Primary Key
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string Description { get; set; }
+        public string Category { get; set; }
+    }
+}
